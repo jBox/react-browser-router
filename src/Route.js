0aca6d38e6cec7c88c6ac8f856b4a21fd5916b2b
Junhua Wei <junhua@outlook.com>
2017-10-24 11:03:30 +0800

    Advance change for Route & v2.0

--- src/Route.js
+++ src/Route.js
@@ -1,15 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router";
 
-export default (route) => {
+export default (route) => {    
+    const routes = route.routes;
     const routeProps = {
         path: route.path,
         exact: route.exact,
-        strict: route.strict
+        strict: route.strict,
+        location: route.location
     };
 
-    return (<Route {...routeProps} render={(props) =>
-        //pass the sub-routes down to keep nesting
-        (<route.component {...props} routes={route.routes} />)
-    } />);
+    // https://reacttraining.com/react-router/web/api/Route/Route-props
+    // render props: { match, lacation, history }
+    if (route.component) {
+        routeProps.render = (props) => (<route.component {...props} routes={routes} />);
+    } else if (route.render) {
+        routeProps.render = (props) => (route.render({ ...props, routes }));
+    } else if (route.children) {
+        routeProps.children = (props) => (route.children({ ...props, routes }));
+    }
+
+    return (<Route {...routeProps} />);
 };
