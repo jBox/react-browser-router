bfe4be53b025ec86891cd0059725419685c16929
Junhua Wei <junhua@outlook.com>
2018-01-09 10:20:46 +0800

    route supports authorize

--- src/AdvancedRoute.js
+++ src/AdvancedRoute.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route, Redirect } from "react-router";
 
-export default (route) => {    
+const DEFAULT_AUTH = () => ({ verified: true });
+
+const infoRedirect = (redirect, location) => {
+    const from = `${location.pathname}${location.search}`;
+    const to = (redirect || "").replace(/{\d+}/ig, encodeURIComponent(from));
+    return { to, from };
+};
+
+export default (route) => {
     const routes = route.routes;
+    const authorize = route.authorize || DEFAULT_AUTH;
     const routeProps = {
         path: route.path,
         exact: route.exact,
@@ -13,11 +22,35 @@ export default (route) => {
     // https://reacttraining.com/react-router/web/api/Route/Route-props
     // render props: { match, lacation, history }
     if (route.component) {
-        routeProps.render = (props) => (<route.component {...props} routes={routes} />);
+        routeProps.render = (props) => {
+            const auth = authorize(props);
+            if (!auth.verified) {
+                const { location } = props;
+                const redirect = infoRedirect(auth.redirect, location);
+                return (<Redirect {...redirect} />);
+            }
+            return (<route.component {...props} routes={routes} />);
+        };
     } else if (route.render) {
-        routeProps.render = (props) => (route.render({ ...props, routes }));
+        routeProps.render = (props) => {
+            const auth = authorize(props);
+            if (!auth.verified) {
+                const { location } = props;
+                const redirect = infoRedirect(auth.redirect, location);
+                return (<Redirect {...redirect} />);
+            }
+            return (route.render({ ...props, routes }));
+        };
     } else if (route.children) {
-        routeProps.children = (props) => (route.children({ ...props, routes }));
+        routeProps.children = (props) => {
+            const auth = authorize(props);
+            if (!auth.verified) {
+                const { location } = props;
+                const redirect = infoRedirect(auth.redirect, location);
+                return (<Redirect {...redirect} />);
+            }
+            return (route.children({ ...props, routes }));
+        };
     }
 
     return (<Route {...routeProps} />);
